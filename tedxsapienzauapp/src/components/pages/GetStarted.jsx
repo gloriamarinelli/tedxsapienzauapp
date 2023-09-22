f9af114368a053015401e7cc9b5a961daeee53cb
stedagne <stedagnese@gmail.com>
2023-09-22 02:53:44 +0200

    get started provvisorio

--- tedxsapienzauapp/src/components/pages/GetStarted.jsx
+++ tedxsapienzauapp/src/components/pages/GetStarted.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, ImageBackground, Pressable,Dimensions} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+
+
+const PlaceholderImage = require('../images/_106396958_ted.jpg');
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
 const GetStarted = ({ navigation }) => {
   const handlePress = () => {
@@ -10,24 +16,119 @@ const GetStarted = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Button
-        title="Go to partners"
-        onPress={() => navigation.navigate("Partners")}
-      />
+      <View style={styles.imageContainer}>
+        <ImageBackground source={PlaceholderImage} style={styles.image}>
+          <Text style={styles.text}>Ideas • Worth • Spreading</Text>
+          <Text style={styles.text1}>___________________________</Text>
+          <Text style={styles.text2}>altre info qui</Text>
+          <View style={styles.buttonContainer}>
+            <Pressable style={styles.button} onPress={() => navigation.navigate("Partners")}>
+              <Text style={styles.buttonLabel}>{'Get Started'}</Text>
+            </Pressable>
+          </View>
+        </ImageBackground>
+      </View>
     </View>
   );
 };
 
+
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "white",
+    display: 'flex',
+    backgroundColor: "black",
     justifyContent: "center",
     alignItems: "center",
   },
   title: {
     fontSize: 24,
     marginBottom: 20,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+  },
+  text: {
+    fontSize: 36,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.5,
+    color: 'white',
+    fontFamily: 'Helvetica',
+    bottom: 300,
+    position: 'absolute',
+  },
+  text1: {
+    fontSize: 36,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.5,
+    color: 'white',
+    fontFamily: 'Helvetica',
+    bottom: 270,
+    position: 'absolute',
+  },
+  text2: {
+    fontSize: 30,
+    lineHeight: 21,
+    letterSpacing: 0.5,
+    color: 'white',
+    fontFamily: 'Helvetica',
+    bottom: 215,
+    position: 'absolute',
+  },
+
+  imageContainer: {
+    display: 'flex',
+    paddingTop: 58,
+  },
+  image: {
+    width: windowWidth,
+    height: windowHeight,
+    borderRadius: 1,
+    position: 'relative',
+  },
+  buttonContainer: {
+    display: 'flex',
+    width: 320,
+    height: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 4, borderColor: "white", borderRadius: 18,
+    bottom: 60,
+    position: 'absolute',
+    marginHorizontal: 'auto',
+
+  },
+  button: {
+    display: 'flex',
+    borderRadius: 10,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: "red",
+    borderRadius: 18 ,
+  },
+  buttonIcon: {
+    paddingRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonLabel: {
+    color: 'white',
+    fontSize: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingRight: 15,
+    textAlign:'center',
+    fontFamily: 'Helvetica',
+    fontWeight: 'bold',
   },
 });
 
