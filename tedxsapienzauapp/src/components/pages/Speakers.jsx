d5ee9fb9034ae1fdd418d19b156f5f7f1a392a1a
Simone Teglia <simone.teglia9@gmail.com>
2023-10-13 15:47:43 +0200

    Speaker card structure

--- tedxsapienzauapp/src/components/pages/Speakers.jsx
+++ tedxsapienzauapp/src/components/pages/Speakers.jsx
@@ -6,26 +6,59 @@ import {
   StyleSheet,
   Dimensions,
   ScrollView,
+  ImageBackground,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { RFValue } from "react-native-responsive-fontsize";
+import { LinearGradient } from "expo-linear-gradient";
+
 const windowWidth = Dimensions.get("window").width;
+const PlaceholderImage = require("../images/Red-Circle.png");
 
 const Speaker = () => {
-  const SpeakerCard = ({ name, image, description }) => {
+  const SpeakerCard = ({ name, image, description, time }) => {
     return (
       <View
         style={{
           width: "100%",
           height: 250,
-          backgroundColor: "red",
+          backgroundColor: "#000",
+          position: "relative",
         }}
       >
         <Image
           source={image}
-          style={{ position: "absolute", maxHeight: 250, width: "100%" }}
+          style={{
+            position: "absolute",
+            maxHeight: 250,
+            width: "100%",
+            borderRadius: 10,
+          }}
         />
-        <Text style={styles.text1}>{name}</Text>
+        <LinearGradient
+          style={{ width: "100%", height: "100%", position: "absolute" }}
+          colors={["transparent", "rgba(0, 0, 0, 0.8)"]}
+          locations={[0, 0.85]}
+        >
+          <Text style={styles.text1}>{name}</Text>
+          <ImageBackground
+            source={PlaceholderImage}
+            style={{
+              position: "absolute",
+              right: 5,
+              top: 5,
+              width: 80,
+              aspectRatio: 1,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Text style={{ color: "#fff", fontSize: 18, fontWeight: "bold" }}>
+              {time}
+            </Text>
+          </ImageBackground>
+        </LinearGradient>
       </View>
     );
   };
@@ -34,8 +67,12 @@ const Speaker = () => {
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <SpeakerCard
-          name={"Nome Cognome"}
+          name={"Rose Villain"}
           image={require("../images/speakers/rose_villain.jpg")}
+          description={
+            "lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+          }
+          time={"17:00"}
         />
       </ScrollView>
     </SafeAreaView>
@@ -63,6 +100,10 @@ const styles = StyleSheet.create({
     fontSize: RFValue(24),
     color: "white",
     textAlign: "center",
+    fontWeight: "bold",
+    position: "absolute",
+    bottom: 5,
+    left: 10,
   },
   image: {
     width: windowWidth,
