f73d8ef295a7945bedc19b91b09d5dd7ba17f702
quellocinese <51003235+PhilHippo@users.noreply.github.com>
2023-11-04 13:57:52 +0100

    smaller map

--- tedxsapienzauapp/src/components/pages/Map.jsx
+++ tedxsapienzauapp/src/components/pages/Map.jsx
@@ -11,13 +11,13 @@ const Map = () => {
       <ScrollView>
         <View style={styles.mapContainer}>
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23752.480093043745!2d12.473118594711794!3d41.91306862757848!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13258f5b0fe7d911%3A0xed5342b156dcfd2b!2sSapienza%20University%20of%20Rome!5e0!3m2!1sen!2snl!4v1699010054930!5m2!1sen!2snl"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2969.4928590518475!2d12.511863475793051!3d41.90376257123888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13258f5b0fe7d911%3A0xed5342b156dcfd2b!2sSapienza%20University%20of%20Rome!5e0!3m2!1sen!2snl!4v1699102404041!5m2!1sen!2snl"
             width={windowWidth - 100}
             height={300}
             style={styles.map}
             allowFullScreen
             loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
+            referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
         </View>
         <Text style={styles.text}>
