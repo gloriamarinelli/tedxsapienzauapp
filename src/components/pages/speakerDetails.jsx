c73b8674f4170c886f8527dd5b3bab734c89396a
Gloria Marinelli <gloria.marinelli123@gmail.com>
2025-03-31 23:14:47 +0200

    update descrizione, bio, titolo speakers

--- src/components/pages/speakerDetails.jsx
+++ src/components/pages/speakerDetails.jsx
@@ -5,13 +5,18 @@ export const speaker24 = [
     name: "Alessia Dapoto",
     image: require("../images/speakers24/dapoto.png"),
     time: "10:40",
-    speechTitle: "Titolo 1",
+    speechTitleIta:
+      "Come cambiare te stesso e la società grazie a una nuova materia scolasticaa",
+    speechTitleEng:
+      "How to change yourself and society thanks to a new school subject",
     bioIta:
       "23 anni, studentessa al corso internazionale di medicina della Sapienza. Ha vissuto tra Francia, Germania e Italia, dove ha studiato e fatto ricerca. Scrittrice, poliglotta, attrice, musicista, past president un’associazione giovanile del Rotary International e membro dello European Youth Parliament.",
-    bioEng: "...",
+    bioEng:
+      "23 years old, student in the international medical program at Sapienza University. She has lived in France, Germany, and Italy, where she studied and did research. Writer, polyglot, actress, musician, former president of a youth Rotary International association, and member of the European Youth Parliament.",
     resumeIta:
       "Esploreremo un paradosso affascinante: cerchiamo tante esperienze nel mondo esterno ma a volte trascuriamo il viaggio più importante, quello interiore. Ci accompagnerà tra neuroscienze e medicina per scoprire come l’educazione emotiva può trasformare noi stessi e la società, rivoluzionando il mondo.",
-    resumeEng: "...",
+    resumeEng:
+      "The paradox: we seek countless experiences in the external world, yet often neglect the most important journey of all — the one inside ourselves. She will guide us through neuroscience and medicine to discover how emotional education can transform both ourselves and society, being a revolution for the world.",
     speechIta: "discorso in italiano",
     speechEng: "discorso in inglese",
   },
@@ -20,11 +25,16 @@ export const speaker24 = [
     name: "Nicola Armaroli ",
     image: require("../images/speakers24/armaroli.webp"),
     time: "11:00",
-    speechTitle: "Titolo 2",
-    bioIta: "Bio in italiano ",
-    bioEng: "Bio in inglese",
-    resumeIta: "...",
-    resumeEng: "",
+    speechTitleIta: "La transizione energetica è una fregatura.",
+    speechTitleEng: "Title 2",
+    bioIta:
+      "Nicola Armaroli è un chimico, dirigente di ricerca presso il CNR, direttore della rivista Sapere e membro dell'Accademia Nazionale delle Scienze. La sua attività scientifica riguarda fotochimica e fotofisica. Studia inoltre la transizione del sistema energetico globale verso modelli più sostenibili.",
+    bioEng:
+      "Nicola Armaroli is a chemist, research director at the CNR, editor of the journal Sapere, and a member of the National Academy of Sciences. His scientific work focuses on photochemistry and photophysics. He also studies the transition of the global energy system towards more sustainable models.",
+    resumeIta:
+      "Pochi anni fa, ci siamo resi conto delle nostre debolezze di fronte ai cambiamenti della biosfera ed eravamo d'accordo sul dover prendere azioni decise. Oggi, la transizione energetica sembra non più necessaria e ci sentiamo dire che stanno solo cercando di fregarci. Perché il racconto è cambiato? ",
+    resumeEng:
+      "A few years ago, we realized our weaknesses in the face of changes in the biosphere, and we agreed that decisive action was needed. Today, the energy transition no longer seems necessary, and we are being told that they are just trying to deceive us. Why has the narrative changed?",
     speechIta: "discorso in italiano",
     speechEng: "discorso in inglese",
   },
@@ -33,10 +43,11 @@ export const speaker24 = [
     name: "Abbozzo",
     image: require("../images/speakers24/abbozzo.webp"),
     time: "11:20",
-    speechTitle: "Titolo 3",
-    bioIta: "Bio in italiano ",
-    bioEng: "Bio in inglese",
-    resumeIta: "...",
+    speechTitleIta: "Tante Eccezioni Decentralizzate",
+    speechTitleEng: "Many Decentralized Exceptions",
+    bioIta: "Abbozzo, in arte Matteo Abozzi, classe 2000, anche se alle medie stava nella classe 3B. Ha studiato alla scuola elementare di Springfield, ha superato i test Gufo ed ha acquisito la Forza. Gli piace il blues e il latte freddo. Prima o poi morirà.",
+    bioEng: "Abbozzo, aka Matteo Abozzi, class of 2000, although his middle school class was the 3B. He studied at Springfield Elementary School, passed the O.W.L tests and acquired the Force. He likes blues and cold milk. Sooner or later he will die",
+    resumeIta: "",
     resumeEng: "",
     speechIta: "discorso in italiano",
     speechEng: "discorso in inglese",
@@ -46,10 +57,14 @@ export const speaker24 = [
     name: "Lorenzo Nardi",
     image: require("../images/speakers24/nardi.webp"),
     time: "11:40",
-    speechTitle: "Titolo 4",
-    bioIta: "Bio in italiano ",
+    speechTitleIta:
+      "Lo spazio è qui: il suo impatto nella nostra vita quotidiana",
+    speechTitleEng: "Title 2",
+    bioIta:
+      "Lorenzo, pilota dell’Aeronautica Militare e ingegnere aerospaziale, ha completato l’iter da Ufficiale nel 6° Stormo di Ghedi. Laureato in discipline aerospaziali, svolge un Dottorato alla Sapienza. Collabora a progetti ASI, NASA ed UE, con un esperimento in lancio sulla ISS nel 2025.",
     bioEng: "Bio in inglese",
-    resumeIta: "...",
+    resumeIta:
+      "Perché investire nella ricerca aerospaziale quando abbiamo così tanti problemi qui sulla Terra? Spazio e Terra sono indissolubilmente connessi: Nardi, pilota e ingegnere aerospaziale, ci mostra come le innovazioni sviluppate per i cieli trovano applicazioni cruciali nella nostra vita quotidiana",
     resumeEng: "",
     speechIta: "discorso in italiano",
     speechEng: "discorso in inglese",
@@ -65,7 +80,8 @@ export const speaker24 = [
     name: "Lorenzo Saltarelli",
     image: require("../images/speakers24/saltarelli.webp"),
     time: "12:20",
-    speechTitle: "Titolo 6",
+    speechTitleIta: "Titolo 2",
+    speechTitleEng: "Title 2",
     bioIta: "Bio in italiano ",
     bioEng: "Bio in inglese",
     resumeIta: "...",
@@ -78,7 +94,8 @@ export const speaker24 = [
     name: "Esmeralda Moretti",
     image: require("../images/speakers24/moretti.webp"),
     time: "12:40",
-    speechTitle: "Titolo 7",
+    speechTitleIta: "Titolo 2",
+    speechTitleEng: "Title 2",
     bioIta: "Bio in italiano ",
     bioEng: "Bio in inglese",
     resumeIta: "...",
@@ -91,8 +108,10 @@ export const speaker24 = [
     name: "Pepa Pasatu",
     image: require("../images/speakers24/pasatu.png"),
     time: "13:00",
-    speechTitle: "Titolo 8",
-    bioIta: "Bio in italiano ",
+    speechTitleIta: "Titolo 2",
+    speechTitleEng: "Want Electrifying Sex? Drop the Act, Feel the Pleasure.",
+    bioIta:
+      "Pepa Pasatu esplora il piacere sessuale e la consapevolezza corporea. Di origine berlinese, ha vissuto e studiato in Italia, Argentina e Austria. Con una Laurea in Psicologia (Università di Potsdam) e un Master in Psicosessuologia Clinica (Sapienza, Roma), ricerca la sessualità, si esibisce e conduce workshop.",
     bioEng:
       "Pepa Pasatu explores sexual pleasure and body awareness. Born in Berlin, Pepa has lived and learned in Italy, Argentina, and Austria. Holding a BSc in Psychology (University of Potsdam) and an MSc in Clinical Psychosexology (Sapienza, Rome), Pepa researches sexuality, performs, and leads workshops.",
     resumeIta: "...",
@@ -112,11 +131,16 @@ export const speaker24 = [
     name: "Riccardo Azzali",
     image: require("../images/speakers24/azzali.png"),
     time: "15:00",
-    speechTitle: "Titolo 10",
-    bioIta: "Bio in italiano ",
-    bioEng: "Bio in inglese",
-    resumeIta: "...",
-    resumeEng: "",
+    speechTitleIta: "Il fascino dei paradossi",
+    speechTitleEng: "The charm of paradoxes",
+    bioIta:
+      "Riccardo Azzali è un fisico teorico è un divulgatore scientifico molto attivo sui social media. È noto per la sua capacità di spiegare concetti complessi di scienza e filosofia in modo semplice e accessibile, spesso trattando dilemmi filosofici e paradossi.",
+    bioEng:
+      "Riccardo Azzali is a theoretical physicist and a science communicator on social media. He is known for his ability to explain complex concepts in science and philosophy in a simple and accessible manner, often addressing philosophical dilemmas and paradoxes",
+    resumeIta:
+      "Crediamo che i paradossi siano un’assurdità incomprensibile, ma in realtà sono motori di scoperta. Attraverso i paradossi più straordinari, Riccardo Azzali ci dimostra che dobbiamo accogliere le contraddizioni, perché è lì che la logica trova i suoi stimoli.",
+    resumeEng:
+      "We believe that paradoxes are an absurdity, but they are engines of discovery. Through the most extraordinary paradoxes, Riccardo Azzali shows us that we must embrace contradictions because that is where logic finds its inspiration.",
     speechIta: "discorso in italiano",
     speechEng: "discorso in inglese",
   },
@@ -125,13 +149,17 @@ export const speaker24 = [
     name: "Carmen Panepinto Zayati",
     image: require("../images/speakers24/panepinto.webp"),
     time: "15:20",
-    speechTitle: "Titolo 11",
+    speechTitleIta:
+      "Perché il futuro delle STEM ha bisogno di TE (Sì, proprio di te!)",
+    speechTitleEng: "Why the Future of STEM Needs YOU (Yes, You!)",
     bioIta:
       "Carmen Panepinto Zayati, ingegnere bionico e Miss Universe Italy 2023, sfida le barriere psicologiche nelle materie STEM. Dimostra che fascino e intelligenza possono coesistere, ispirando le nuove generazioni a rompere gli schemi. La sua storia ispira le nuove generazioni a superare i pregiudizi e i ruoli di genere.",
-    bioEng: "Bio in inglese",
+    bioEng:
+      "Carmen conducts research in Bionic Engineering at the Scuola Superiore Sant’Anna. She is Miss Universe Italy 2023, one of the ambassadors of the STEM Law, and a STEM ambassador for Samsung Italy. She brings science to schools and shares knowledge at events, on TV, and on social media, breaking stereotypes and inspiring young people to embrace science.",
     resumeIta:
-      "Le barriere psicologiche che avvolgono le STEM e cosa succederebbe se le eliminassimo. Se già la tecnologia sembra fantascientifica, cosa succederebbe se più persone contribuissero all’innovazione tecnologica?",
-    resumeEng: "",
+      "Le STEM non sono solo per i geni, ma una cassetta degli attrezzi per tutti. Eppure, tendiamo a incasellare le persone, decidendo chi appartiene a cosa. E se abbattere queste barriere permettesse la prossima grande scoperta? Mettiamo in discussione i limiti e ripensiamo chi può costruire il futuro",
+    resumeEng:
+      "STEM isn’t just for geniuses, it’s a toolbox for everyone. Yet, we put people into boxes, shaping who belongs where. What if breaking those barriers unleashed the next big breakthrough? Let’s challenge the limits and rethink who gets to build the future.",
     speechIta: "discorso in italiano",
     speechEng: "discorso in inglese",
   },
@@ -140,13 +168,16 @@ export const speaker24 = [
     name: "Kento",
     image: require("../images/speakers24/kento.png"),
     time: "15:40",
-    speechTitle: "Titolo 12",
+    speechTitleIta: "Una contro-opinione sul carcere minorile.",
+    speechTitleEng: "A counter-opinion on juvenile detention.",
     bioIta:
       "Kento, pseudonimo di Francesco Carlo, è un rapper originario di Reggio Calabria in cui inizia la carriera agli inizi degli anni 90 diventando l’esponente di un genere di rap di denuncia, impegnato nella lotta contro le ingiustizie sociali, la discriminazione, i diritti umani e la politica.",
-    bioEng: "Bio in inglese",
+    bioEng:
+      "Kento, stage name of Francesco Carlo, is a rapper from Reggio Calabria who started his career in the early 90s. He became a prominent figure in a type of rap focused on social activism, fighting against social injustice, discrimination, human rights, and politics.",
     resumeIta:
       "Le immagini che abbiamo del carcere minorile sono spesso sbagliate. Opinioni diffuse nella società, media e fiction alimentano una narrazione irrealistica. Serve un cambiamento nella prospettiva e nelle opportunità offerte ai ragazzi, partendo dal ricordarci che dietro ogni errore c’è un essere umano.",
-    resumeEng: "",
+    resumeEng:
+      "The idea we have of juvenile detention centers is often inaccurate. General opinions in society, media, and fiction fuel an unrealistic narrative. A change in perspective and in the opportunities offered to these young men and women is needed, starting with the reminder that behind every mistake, there is a human being.",
     speechIta: "discorso in italiano",
     speechEng: "discorso in inglese",
   },
